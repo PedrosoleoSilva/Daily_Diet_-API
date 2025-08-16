de5b3d04bb2f2dc9697f70e010178ba0f5fd6d33
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2025-08-16 15:32:25 -0300

    Criando metodo criar dieta

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.dieta import Diet
 
 app = Flask(__name__)
+
+diets = []
+id_diet = 1
+
+@app.route("/diet", methods=["POST"])
+def create_diet():
+    global id_diet
+    data = request.get_json()
+    nova_diet = Diet(id = id_diet, nome = data.get("nome"), descricao = data.get("descricao"))
+    id_diet += 1
+    diets.append(nova_diet)
+    print(nova_diet)
+    return jsonify({"message": "Dieta criada com sucesso!"})
+
 
 
 if __name__ == "__main__":
