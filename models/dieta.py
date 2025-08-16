af3b8ee74a465309057617b2fc5f9e36ebb67de2
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2025-08-16 15:26:15 -0300

    Criando class Diet

---
+++ models/dieta.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+class Diet:
+    def __init__(self, id, nome, descricao, data_hora):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
+        self.data_hora = datetime.now()
+
+    def to_dict(self):
+        return {
+                    "id": self.id,
+                    "nome": self.nome,
+                    "descricao": self.descricao,
+                    "data_hora": self.data_hora.strftime("%Y-%m-%d %H:%M:%S")
+                }
+        
